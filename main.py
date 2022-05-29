3b95043d1eee0366019ea30999a0529bb5f68bb0
Ekene C <clarencekene@outlook.com>
2022-05-29 23:15:04 +0100

    updated dictionary items

--- main.py
+++ main.py
@@ -4,13 +4,28 @@
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
+    # [assignment] Add your code here
+    global f
+    f = open(filename, 'r')
     return "Hello World"
 
+print(read_file_content(filename='./story.txt'))
 
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
+    x = f.read()
+    for char in '.,?!':
+        x = x.replace(char, '')
+    y = x.split()
 
-    return {"as": 10, "would": 20}
+    for item in y:
+        thisDict = {
+            item : y.count(item)
+        }
+
+        print (thisDict)
+
+    #return {"as": 10, "would": 20}
+
+count_words() 
